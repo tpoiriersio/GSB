c9cf3c39f385276f18afdab977441b39291278c7
afloremont <alanfloremont06@gmail.com>
2019-09-10 17:21:54 +0200

    Ajout du fond hyper bo

--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,15 @@ namespace GSB
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void PictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
